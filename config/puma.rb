3dd7f1b04dc1ff0b6fad7b8ea5669e888d8d2f50
Mirko Akov <mupkoo@gmail.com>
2019-08-07 09:44:32 +0300

    Some more changes

--- config/puma.rb
+++ config/puma.rb
@@ -10,5 +10,5 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-    ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
